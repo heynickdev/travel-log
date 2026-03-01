a85976efb1d15cbdafb2181171a01fcc829cb0f4
Nick <nickgirling76@gmail.com>
2026-03-01 15:24:42 +0000

    :sparkes: fixed the lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import antfu from "@antfu/eslint-config";
+import antfu from "@antfu/eslint-config"
 // @ts-check
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import withNuxt from "./.nuxt/eslint.config.mjs"
 
 export default withNuxt(
   antfu(
@@ -60,4 +60,4 @@ export default withNuxt(
       },
     },
   ),
-);
+)
