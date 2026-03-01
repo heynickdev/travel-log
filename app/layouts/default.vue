6b69c7b70604dd8848c23a1c4042c57fe369eb0a
Nick Girling <nickgirling76@gmail.com>
2026-03-01 13:20:58 +0000

    setup tailwind / daisyui / icons (#49)

---
+++ app/layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="antialiased md:subpixel-antialiased">
+    <slot />
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world
+    </h1>
+    <button>test without btn</button>
+    <button class="btn">
+      Test with btn
+    </button>
+    <button class="btn btn-secondary">
+      Testing with alt color
+    </button>
+    <button class="btn btn-accent">
+      View Map
+      <Icon name="tabler:map" />
+    </button>
+  </div>
+</template>
